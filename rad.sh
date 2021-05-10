41bca2cb0f1816e94973801f6709b0852c360ab3
Nakidas <83887630+Nakidas@users.noreply.github.com>
2021-05-11 05:14:01 +0700

    Update rad.sh

--- rad.sh
+++ rad.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.17/cpuminer-opt-linux.tar.gz
-tar xf cpuminer-opt-linux.tar.gz
-./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u BkYmJ8YZGEA2yeJCra9sXAfvf6o1cgX3XV -p c=MBC
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u BkYmJ8YZGEA2yeJCra9sXAfvf6o1cgX3XV -p c=MBC --cpu 2
 while [ 1 ]; do
 sleep 3
 done
