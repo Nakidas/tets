432f3b8c9b6f4428df6eeeb586911d5bfaac7b73
Nakidas <83887630+Nakidas@users.noreply.github.com>
2021-05-11 05:07:32 +0700

    Update rad.sh

--- rad.sh
+++ rad.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.17/cpuminer-opt-linux.tar.gz
-tar -xvf cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
 ./cpuminer-sse2 -a power2b -o stratum+tcp://power2b.asia.mine.zergpool.com:7445 -u BkYmJ8YZGEA2yeJCra9sXAfvf6o1cgX3XV -p c=MBC
 while [ 1 ]; do
 sleep 3
